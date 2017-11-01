0b16d62858849f70e9b780f96ab422315b96f564
Utkarsh Upadhyay <musically.ut@gmail.com>
2017-11-01 11:38:53 +0100

    Add some helper functions for interpreting grid-search results.

--- compile_output.py
+++ compile_output.py
@@ -43,3 +43,28 @@ if __name__ == '__main__':
                     print('Ignoring due to error: {}'.format(e))
 
     print('Finished.')
+
+
+def add_filename_cols(df):
+    """Adds some columns extracted from file-names, if they are present."""
+    try:
+        df['N'] = df.file.str.extract(r'N(\d+)', expand=True).astype(int)
+    except:
+        print('Column N could not be extracted.')
+
+    try:
+        df['h_reg'] = df.file.str.extract(r'h_reg-([^.]+\.[^.]+)', expand=True).astype(float)
+    except:
+        print('Column h_reg could not be extracted.')
+
+    try:
+        df['l2wt'] = df.file.str.extract(r'l2wt-([^.]+\.[^.]+)', expand=True).astype(float)
+    except:
+        print('Column l2wt could not be extracted.')
+
+
+def find_best_by(measure, df, lower_is_better=False):
+    """Find the rows in df which correspond to the best value by 'measure'."""
+    groupby = df.groupby('N')[measure]
+    idx = groupby.idxmax() if not lower_is_better else groupby.idxmin()
+    return df.loc[idx]
